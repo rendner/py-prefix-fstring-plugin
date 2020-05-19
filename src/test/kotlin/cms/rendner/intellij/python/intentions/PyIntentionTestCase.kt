e365f165e8415442dbe3a46affb7ed900925d34d
Daniel Schmidt <rendner@users.noreply.github.com>
2020-05-20 01:48:01 +0200

    simplify setting of the language level

--- src/test/kotlin/cms/rendner/intellij/python/intentions/PyIntentionTestCase.kt
+++ src/test/kotlin/cms/rendner/intellij/python/intentions/PyIntentionTestCase.kt
@@ -1,9 +1,7 @@
 package cms.rendner.intellij.python.intentions
 
-import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiFile
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
-import com.intellij.testFramework.fixtures.TempDirTestFixture
-import com.intellij.testFramework.fixtures.impl.LightTempDirTestFixtureImpl
 import com.jetbrains.python.psi.LanguageLevel
 
 /**
@@ -11,49 +9,29 @@ import com.jetbrains.python.psi.LanguageLevel
  */
 abstract class PyIntentionTestCase : BasePlatformTestCase() {
 
-    private var requestedLanguageLevel: LanguageLevel = LanguageLevel.PYTHON27
-
     fun doTest(hint: String, languageLevel: LanguageLevel) {
-        runWithLanguageLevel(languageLevel) {
-            val testFileName = getTestName(true)
-            myFixture.configureByFile("$testFileName.py")
-            myFixture.launchAction(myFixture.findSingleIntention(hint))
-            myFixture.checkResultByFile(testFileName + "_after.py", true)
-        }
+        val testFileName = getTestName(true)
+        val file = myFixture.configureByFile("$testFileName.py")
+        injectLanguageLevel(file, languageLevel)
+        myFixture.launchAction(myFixture.findSingleIntention(hint))
+        myFixture.checkResultByFile(testFileName + "_after.py", true)
     }
 
     protected open fun doNegativeTest(hint: String, languageLevel: LanguageLevel) {
-        runWithLanguageLevel(languageLevel) {
-            val testFileName = getTestName(true)
-            myFixture.configureByFile("$testFileName.py")
-            assertEmpty(myFixture.filterAvailableIntentions(hint))
-        }
+        val testFileName = getTestName(true)
+        val file = myFixture.configureByFile("$testFileName.py")
+        injectLanguageLevel(file, languageLevel)
+        assertEmpty(myFixture.filterAvailableIntentions(hint))
     }
 
-    private fun runWithLanguageLevel(languageLevel: LanguageLevel, runnable: () -> Unit) {
-        requestedLanguageLevel = languageLevel
-        try {
-            runnable.invoke()
-        } finally {
-            requestedLanguageLevel = LanguageLevel.PYTHON27
-        }
-    }
+    private fun injectLanguageLevel(file: PsiFile, languageLevel: LanguageLevel) {
+        /*
+        This is a hack to set the expected language level.
 
-    override fun createTempDirTestFixture(): TempDirTestFixture {
-        return object : LightTempDirTestFixtureImpl(true) {
-            override fun createFile(targetPath: String): VirtualFile {
-                /*
-                This is a hack to set the expected language level.
-
-                The language level in "PyPrefixFStringIntention" is determined via "LanguageLevel.forElement(psiElement)".
-                Somewhere down the road "PyUtil.getLanguageLevelForVirtualFile(this.getProject(), virtualFile)" is called
-                which fetches the language level from the parent of "virtualFile" via "getUserData(LanguageLevel.KEY)".
-                The "virtualFile" is the file which is created in this method.
-                 */
-                val file = super.createFile(targetPath)
-                file.parent.putUserData(LanguageLevel.KEY, requestedLanguageLevel)
-                return file
-            }
-        }
+        The language level in "PyPrefixFStringIntention" is determined via "LanguageLevel.forElement(psiElement)".
+        Somewhere down the road "PyUtil.getLanguageLevelForVirtualFile(this.getProject(), virtualFile)" is called
+        which fetches the language level from the parent of "virtualFile" via "getUserData(LanguageLevel.KEY)".
+         */
+        file.virtualFile.parent.putUserData(LanguageLevel.KEY, languageLevel)
     }
 }
